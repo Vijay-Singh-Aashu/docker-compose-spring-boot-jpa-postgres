72e1d6c7def6073256efcd043e72b39f89e80983
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-06 18:59:23 +0530

    create-docker-image-with-postgres

---
+++ bezkoder-app/src/main/java/com/excellence/spring/jpa/postgresql/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.excellence.spring.jpa.postgresql.repository;
+
+import java.util.List;
+
+import com.excellence.spring.jpa.postgresql.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
